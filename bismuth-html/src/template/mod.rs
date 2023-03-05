f96e234af6513860c1d80703edd22d7c42ab1044
YummyOreo <bobgim20@gmail.com>
2023-03-04 18:30:15 -0600

    adds setup for template

--- bismuth-html/src/template/mod.rs
+++ bismuth-html/src/template/mod.rs
@@ -1,14 +1,35 @@
+#![allow(unused, dead_code)]
+use bismuth_parser::{
+    custom::CustomElm,
+    tree::{Element, Kind},
+};
+use std::collections::HashMap;
+
 use crate::render::Render;
 
-pub struct Template {}
+/// Data/Values: Will be replaced (as a string) at {key}
+/// Reserved keys: `body` and `elements`
+///
+/// Body: Will be replaced at {body}
+/// Elements: Will be replaced at {elements}
+pub struct Template<'a> {
+    values: &'a HashMap<String, String>,
+    body: &'a Option<String>,
+    elements: &'a Vec<Element>,
+}
+// ^ should all be references to stuff
 
-impl Template {
-    pub fn new() -> Self {
-        Self {}
+impl<'a> Template<'a> {
+    pub fn new(elm: &'a Element, c: &'a CustomElm) -> Self {
+        Self {
+            values: &c.values,
+            body: &c.body,
+            elements: &elm.elements,
+        }
     }
 }
 
-impl Render for Template {
+impl Render for Template<'_> {
     fn render(&mut self) -> String {
         todo!()
     }
