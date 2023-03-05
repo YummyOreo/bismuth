a39d9b41559d0aa1a3ad99cb141f698ce86b06ce
YummyOreo <bobgim20@gmail.com>
2023-03-04 18:40:03 -0600

    better implementation of this

--- bismuth-html/src/template/mod.rs
+++ bismuth-html/src/template/mod.rs
@@ -4,6 +4,7 @@ use bismuth_parser::{
     tree::{Element, Kind},
 };
 use std::collections::HashMap;
+use std::convert::TryFrom;
 
 use crate::render::Render;
 
@@ -12,20 +13,31 @@ use crate::render::Render;
 ///
 /// Body: Will be replaced at {body}
 /// Elements: Will be replaced at {elements}
+#[derive(Debug, PartialEq)]
 pub struct Template<'a> {
+    template: &'a String,
     values: &'a HashMap<String, String>,
     body: &'a Option<String>,
     elements: &'a Vec<Element>,
 }
 // ^ should all be references to stuff
 
-impl<'a> Template<'a> {
-    pub fn new(elm: &'a Element, c: &'a CustomElm) -> Self {
-        Self {
-            values: &c.values,
-            body: &c.body,
-            elements: &elm.elements,
+impl<'a> TryFrom<&'a Element> for Template<'a> {
+    type Error = ();
+
+    /// Try to convert a element that's kind == CustomElement to a Template
+    fn try_from(elm: &'a Element) -> Result<Self, Self::Error> {
+        if let Kind::CustomElement(c) = &elm.kind {
+            if let Some(t) = &c.template {
+                return Ok(Self {
+                    template: t,
+                    values: &c.values,
+                    body: &c.body,
+                    elements: &elm.elements,
+                });
+            }
         }
+        Err(())
     }
 }
 
