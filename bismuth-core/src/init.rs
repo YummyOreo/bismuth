c29894f680d4a0598ebd2d356aa54d2a0b249a56
YummyOreo <bobgim20@gmail.com>
2023-03-18 10:33:47 -0500

    adds more stuff to init feature + tested it

--- bismuth-core/src/init.rs
+++ bismuth-core/src/init.rs
@@ -1,7 +1,7 @@
-use std::fs::create_dir_all;
+use std::fs::{create_dir_all, write};
 use std::path::PathBuf;
 
-const SRC_DIR: &str = "str/";
+const SRC_DIR: &str = "src/";
 const ASSETS_DIR: &str = "assets/";
 
 pub fn init_folder(name: &str) -> Result<(), std::io::Error> {
@@ -9,7 +9,12 @@ pub fn init_folder(name: &str) -> Result<(), std::io::Error> {
     let src = PathBuf::from(format!("{dir}{SRC_DIR}"));
     let assets = PathBuf::from(format!("{dir}{ASSETS_DIR}"));
 
+    let index_file = src.join("index.md");
+    let config_file = PathBuf::from(format!("{dir}/bismuth.toml"));
+
     create_dir_all(src)?;
     create_dir_all(assets)?;
+    write(index_file, "# This is the entry for your website!")?;
+    write(config_file, include_str!("../config.toml"))?;
     Ok(())
 }
