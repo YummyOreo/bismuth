7dbd791ed44890fd22952868b5f0b511f0102ee3
YummyOreo <bobgim20@gmail.com>
2023-03-22 18:56:16 -0500

    more work on the prompter

--- bismuth-tui/src/prompt/mod.rs
+++ bismuth-tui/src/prompt/mod.rs
@@ -2,38 +2,71 @@
 mod input;
 mod select;
 
-pub struct Prompter<T: ToString> {
-    title: T,
-    description: Option<T>,
+#[derive(Debug)]
+pub struct Prompter {
+    title: String,
+    description: Option<String>,
 }
 
 /// For selecting from a list of options
-pub trait Select<T: ToString> {
-    fn get_options(&self) -> Vec<Option<T>>;
+pub trait Select {
+    fn get_options(&self) -> Vec<OptionElement>;
 
     /// Selects a option by the index returned by `get_options`
     /// Returns a string that should be displayed
-    fn select_option(&self, index: i32) -> Option<T>;
+    fn select_option(&mut self, index: i32) -> Option<String>;
+    /// Selects the default option
+    fn select_default(&mut self) -> Option<String>;
 
-    fn get_prompter(&self) -> Prompter<T>;
+    fn get_prompter(&self) -> Prompter;
 
     fn run(&self) {}
 }
 
 /// For accepting string inputs
-pub trait Input<T: ToString> {
+pub trait Input {
     /// Sets the result
     /// Returns a string that should be displayed
-    fn set_result(&self, result: T) -> Option<T>;
+    ///
+    /// This indicates that the input has been chosen
+    fn set_result(&mut self, result: ResultType) -> Option<String>;
+    /// Sets the result to the default result
+    /// Returns a string that should be displayed
+    ///
+    /// This indicates that the user quit
+    fn set_default(&mut self) -> Option<String>;
 
-    fn get_prompter(&self) -> Prompter<T>;
+    fn get_prompter(&self) -> Prompter;
+    fn get_result_type(&self) -> &ResultType;
 
     fn run(&self) {}
 }
 
-pub struct Option<T: ToString> {
-    promt_value: T,
-    promt_description: T,
+#[derive(Debug)]
+pub struct OptionElement {
+    promt_value: String,
+    promt_description: String,
 }
 
-mod utils {}
+pub enum ResultType {
+    Bool(Option<bool>),
+    Other(Option<String>),
+}
+
+mod utils {
+    #![allow(unused_imports)]
+    use crossterm::{event, execute, style};
+    use std::{
+        io::{stdout, Write},
+        time::Duration,
+    };
+
+    /// Reads not blocking
+    /// If returns `None`. That means that there was no event within 250ms
+    pub fn read_event() -> Option<event::Event> {
+        if let Ok(true) = event::poll(Duration::from_millis(250)) {
+            return Some(event::read().expect("Event should not fail"));
+        }
+        None
+    }
+}
