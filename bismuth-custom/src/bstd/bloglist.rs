bbe5fce35e76dad506863abd6342c8a0b157b5ce
YummyOreo <bobgim20@gmail.com>
2023-03-28 11:50:49 -0500

    initial plugin for bloglists still needs tests in demo

--- bismuth-custom/src/bstd/bloglist.rs
+++ bismuth-custom/src/bstd/bloglist.rs
@@ -21,6 +21,59 @@ pub struct BlogList {
     pub id: u32,
 }
 
+impl BlogList {
+    fn get_posts<'a>(
+        &self,
+        files: &[Option<&'a bismuth_parser::Parser>],
+    ) -> Vec<&'a bismuth_parser::Parser> {
+        let mut output_files = vec![];
+        for file in files {
+            if file.is_some() {
+                let file = file.unwrap();
+                if file
+                    .metadata
+                    .frontmatter
+                    .get_path()
+                    .cloned()
+                    .unwrap_or_default()
+                    == self.dir
+                {
+                    output_files.push(file);
+                }
+            }
+        }
+        output_files
+    }
+
+    fn gen_templates(&self, files: &[&bismuth_parser::Parser]) -> Vec<Element> {
+        let mut customs = vec![];
+        for file in files {
+            // get info
+            let frontmatter = &file.metadata.frontmatter;
+            let html_title = frontmatter.get_title().unwrap();
+            let path = frontmatter.get_path().unwrap();
+            let title = frontmatter.get_value("title").unwrap();
+            let date = frontmatter.get_value("date").unwrap();
+
+            let full_path = format!("{path}{html_title}.html");
+
+            // make custom
+            let mut custom = CustomElm::new();
+            custom.name = BLOGITEM_NAME.to_string();
+            custom.values.insert("title".to_string(), title.to_string());
+            custom.values.insert("date".to_string(), date.to_string());
+            custom
+                .values
+                .insert("url".to_string(), full_path.to_string());
+
+            let element = Element::new(Kind::CustomElement(custom));
+
+            customs.push(element);
+        }
+        customs
+    }
+}
+
 impl Plugin for BlogList {
     fn pre_load(&mut self, _: &bismuth_parser::Parser, custom: &crate::Custom) {
         self.values = custom.data.clone();
@@ -28,18 +81,21 @@ impl Plugin for BlogList {
         self.id = custom.id;
     }
 
-    fn run(&mut self, target: &mut bismuth_parser::Parser, _: &[Option<&bismuth_parser::Parser>]) {
+    fn run(
+        &mut self,
+        target: &mut bismuth_parser::Parser,
+        files: &[Option<&bismuth_parser::Parser>],
+    ) {
         let mod_element = target.ast.find_mut(self.id).unwrap();
-        let mut values = self.values.iter().collect::<Vec<(&String, &String)>>();
-        values.sort();
-        for (key, value) in values {
-            let mut custom = CustomElm::new();
-            custom.name = "navbar".to_string();
-            custom.values.insert(key.to_string(), value.to_string());
-            let mut element = Element::new(Kind::CustomElement(custom));
 
-            element.text = Some(format!("{}: {}", key, value));
-            mod_element.elements.push(element);
-        }
+        let posts = self.get_posts(files);
+        let mut customs = self.gen_templates(&posts);
+
+        let mut wrapper = CustomElm::new();
+        wrapper.name = BLOGWRAPPER_NAME.to_string();
+        let mut wrapper_elm = Element::new(Kind::CustomElement(wrapper));
+        wrapper_elm.elements.append(&mut customs);
+
+        mod_element.elements.push(wrapper_elm);
     }
 }
